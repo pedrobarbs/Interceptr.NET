959cd6bef568507e7a0be36e7abc5ef707a698a0
Pedro Farias <pedro.barbosarf@gmail.com>
2022-01-02 13:04:19 -0300

    adjusts

--- TheInterceptor/Program.cs
+++ TheInterceptor/Program.cs
@@ -2,17 +2,16 @@ using TheInterceptor;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
+
 builder.Services.AddScopedIntercepted<ISampleService, SampleService>(new SampleInterceptor());
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
